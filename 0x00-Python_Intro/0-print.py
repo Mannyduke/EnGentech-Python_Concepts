f344101741389f7d3db3dc1764ba6ac7a5785325
Mannyduke <134372511+Mannyduke@users.noreply.github.com>
2023-05-31 13:50:56 +0100

    Update 0-print.py

--- 0x00-Python_Intro/0-print.py
+++ 0x00-Python_Intro/0-print.py
@@ -1,4 +1,4 @@
-file_path = r"C:\my_computer\n"
-website_url = "http:\\engentech.com"
+print(' "This is my file_path C:\\my_computer\\new_line\\ttab.http:\\\\engentech.com" ')
 
-print("This is my file_path " + file_path + "\ttab." + website_url)
+
+
