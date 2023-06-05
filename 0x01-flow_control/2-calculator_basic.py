a5e2904d8b9559fd175410ee4b4bdb763701a5a0
Emmyduke <duke0818828@gmail.com>
2023-06-05 20:48:20 +0100

    second assignment

---
+++ 0x01-flow_control/2-calculator_basic.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    result = x + y
+    return f"The addition of {x} and {y} = {result}"
+def subtract(x, y):
+    result = x - y
+    return f"The subtraction of {x} and {y} = {result}"
+def divide(x, y):
+    if y == 0:
+        return "Error: Cannot divide by zero"
+    result = x / y
+    return f"The division of {x} and {y} = {result}"
+def multiply(x, y):
+    result = x * y
+    return f"The multiplication of {x} and {y} = {result}"
+def mod(x, y):
+    if y == 0:
+        return "Error: Cannot calculate remainder with zero"
+    result = x % y
+    return f"The remainder of {x} divided by {y} = {result}"
+print(add(2, 3))
+print(multiply(6, 2))
+print(divide(6,0))
+print(subtract(5,9))
+
