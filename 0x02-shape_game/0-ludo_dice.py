f0216da7135baa1811ab1cf3ab40519c5bdc3d4b
Mannyduke <134372511+Mannyduke@users.noreply.github.com>
2023-06-16 16:04:24 +0100

    Update 0-ludo_dice.py
    done

--- 0x02-shape_game/0-ludo_dice.py
+++ 0x02-shape_game/0-ludo_dice.py
@@ -4,8 +4,9 @@ while True:
     command = input("Enter a command ('toss' or 'quit' :")
 
     if command == "toss":
-        dice_number = random.randint(1, 6)
-        print("Dice rolled:", dice_number)
+        dice_number = random.randint(1,6)
+        dice2_number = random.randint(1,6)
+        print("Dice rolled:", dice_number ,":" , dice2_number)
     elif command == "quit":
         print("Goodbye and thank you for playing")
         break
